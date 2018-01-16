6acf9db0969457f88fe82d69b87020e5e0a5443f
Joseph Sullivan <youknowjoejoe@gmail.com>
2018-01-16 15:40:18 -0800

    added getVertices method

--- src/math/Polygon.java
+++ src/math/Polygon.java
@@ -13,22 +13,20 @@ public class Polygon {
 	public boolean contains(Vec2 v) {
 		boolean negative = v.minus(vertices[0]).cross(vertices[1].minus(vertices[0])) <0;
 		
+		for(int i = 0; i < vertices.length; i++) {
+			if(v.minus(vertices[i]).cross(vertices[(i+1)%vertices.length]) < 0 == negative) return false;
+		}
+		
 		return true;
+	}
+	
+	public Vec2[] getVertices() {
+		return vertices;
 	}
 	
 	public static Polygon convex(int n) {
 		List<Vec2> vertices = new ArrayList<Vec2>();
 		
 		return new Polygon((Vec2[]) vertices.toArray());
-	}
-	
-	public Vec2[] getVertices(){
-		return vertices;
-	}
-	
-	public void translate(Vec2 v){
-		for(int i = 0; i < vertices.length; i++){
-			vertices[i]=vertices[i].plus(v);
-		}
 	}
 }
